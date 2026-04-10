00469fdff215747da073a52a0c22be68acdbad96
hannahfsee <hannahfrance@student.unimelb.edu.au>
2026-04-10 20:26:02 +1000

    space ship reachability

--- ship.js
+++ ship.js
@@ -8,26 +8,23 @@ export class Ship {
 
   update(keys, dt, clamp, W, H) {
     const mf = 5.5 * dt;
+
     if (keys['a']) this.vx -= mf * 0.08;
     if (keys['d']) this.vx += mf * 0.08;
     if (keys['w']) this.vy -= mf * 0.07;
     if (keys['s']) this.vy += mf * 0.07;
 
-    // Drag
     this.vx *= 0.87;
     this.vy *= 0.87;
 
-    // Velocity cap
     const mv = H * 0.013;
     this.vx = clamp(this.vx, -mv, mv);
     this.vy = clamp(this.vy, -mv, mv);
 
-    // Position
     this.x += this.vx * dt;
     this.y += this.vy * dt;
 
-    // Bounds
-    this.x = clamp(this.x, -W * 0.3,  W * 0.3);
-    this.y = clamp(this.y, -H * 0.24, H * 0.24);
+    this.x = clamp(this.x, -W * 0.42,  W * 0.42);
+    this.y = clamp(this.y, -H * 0.34, H * 0.34);
   }
 }
